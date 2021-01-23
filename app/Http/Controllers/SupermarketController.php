c3e505b0cbd0b6afb5a8b57f7adf263e41746aab
Osvaldo <osvaldo.alvarado.dev@gmail.com>
2021-01-23 16:46:43 -0300

    Crea modelos y controladores

---
+++ app/Http/Controllers/SupermarketController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Supermarket;
+use Illuminate\Http\Request;
+
+class SupermarketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Supermarket  $supermarket
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Supermarket $supermarket)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Supermarket  $supermarket
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Supermarket $supermarket)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Supermarket  $supermarket
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Supermarket $supermarket)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Supermarket  $supermarket
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Supermarket $supermarket)
+    {
+        //
+    }
+}
---
+++ database/factories/SaledetailFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Saledetail;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SaledetailFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Saledetail::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
